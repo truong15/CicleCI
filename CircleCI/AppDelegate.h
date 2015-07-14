c0816433a0feaa42928f1099b2a18ac16a94b444
thinhuv <thinhuv@asiantech.vn>
2015-07-14 09:57:49 +0700

    Init git

---
+++ CircleCI/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CircleCI
+//
+//  Created by ThinhUV-lap on 7/14/15.
+//  Copyright (c) 2015 Thinh Xavi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
